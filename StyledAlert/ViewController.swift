e75fb77cd849429ae0412bfa17b59b3b8fd80d78
Ben Chatelain <ben@octop.ad>
2020-03-30 14:42:06 -0600

    ⬇️ Lower deployment target to 8.0

--- StyledAlert/ViewController.swift
+++ StyledAlert/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func didTapButton(_ sender: Any) {
-        let title = "Title"
-        let message = "first line\n\nsecond line"
+    @IBAction func didTapAlertControllerButton(_ sender: Any) {
+        let title = "Alert Title"
+        let message = "Your request cannot be processed at this time. Please try again later.\n\nError: 123"
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .cancel)
+        alertController.addAction(okAction)
+
         present(alertController, animated: true)
+    }
+
+    @IBAction func didTapDeprecatedAlertButton(_ sender: Any) {
+        let title = "UIAlertView"
+        let message = "⚠️ This class is deprecated."
+
+        let alert = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: "OK")
+        alert.show()
     }
 }
