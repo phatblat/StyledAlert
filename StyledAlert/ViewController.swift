8772fd07bfea398d16620e11c97c93b1b9bfca1a
Ben Chatelain <ben@octop.ad>
2020-03-30 12:11:31 -0600

    Initial Commit

---
+++ StyledAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StyledAlert
+//
+//  Created by Ben Chatelain on 3/30/20.
+//  Copyright © 2020 Log Dash G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
